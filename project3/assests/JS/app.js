353745ef95bad0fc170c4972bf9060175580f301
chirchir <kibet780@gmail.com>
2022-07-09 18:14:04 +0300

    added pages

---
+++ project3/assests/JS/app.js
@@ -0,0 +1,28 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[2000])
+})
+
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+
